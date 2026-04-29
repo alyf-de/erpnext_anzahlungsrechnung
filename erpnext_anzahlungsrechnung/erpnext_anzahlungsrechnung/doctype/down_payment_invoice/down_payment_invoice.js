29ba12ef17bba8994710521d7511a2d8c19ce9f7
Patrick Eissler <77415730+PatrickDEissler@users.noreply.github.com>
2026-04-29 16:53:59 +0200

    feat: create specific DocType "Down Payment Invoice"

---
+++ erpnext_anzahlungsrechnung/erpnext_anzahlungsrechnung/doctype/down_payment_invoice/down_payment_invoice.js
@@ -0,0 +1,71 @@
+// Copyright (c) 2026, ALYF GmbH and contributors
+// For license information, please see license.txt
+
+function base_total(frm) {
+	return flt(frm.doc.total_sales_order_amount);
+}
+
+function sync_amount_from_percentage(frm) {
+	const total = base_total(frm);
+	const pct = flt(frm.doc.down_payment_percentage);
+	if (total) {
+		frm._syncing_down_payment_fields = true;
+		frm.set_value(
+			"down_payment_amount",
+			flt((pct * total) / 100, precision("down_payment_amount", frm.doc))
+		);
+		frm._syncing_down_payment_fields = false;
+	}
+}
+
+function sync_percentage_from_amount(frm) {
+	const total = base_total(frm);
+	const amt = flt(frm.doc.down_payment_amount);
+	if (total) {
+		frm._syncing_down_payment_fields = true;
+		frm.set_value(
+			"down_payment_percentage",
+			flt((amt / total) * 100, precision("down_payment_percentage", frm.doc))
+		);
+		frm._syncing_down_payment_fields = false;
+	}
+}
+
+function resync_after_total_change(frm) {
+	if (frm._syncing_down_payment_fields) {
+		return;
+	}
+	const total = base_total(frm);
+	if (!total) {
+		return;
+	}
+	if (flt(frm.doc.down_payment_percentage)) {
+		sync_amount_from_percentage(frm);
+	} else if (flt(frm.doc.down_payment_amount)) {
+		sync_percentage_from_amount(frm);
+	}
+}
+
+frappe.ui.form.on("Down Payment Invoice", {
+	down_payment_amount(frm) {
+		if (frm._syncing_down_payment_fields) {
+			return;
+		}
+		sync_percentage_from_amount(frm);
+	},
+
+	down_payment_percentage(frm) {
+		if (frm._syncing_down_payment_fields) {
+			return;
+		}
+		sync_amount_from_percentage(frm);
+	},
+
+	sales_order(frm) {
+		frappe.after_ajax(() => resync_after_total_change(frm));
+	},
+
+	total_sales_order_amount(frm) {
+		resync_after_total_change(frm);
+	},
+});
